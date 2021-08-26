30c5298950d5c7ae7512e3356f054b88fdd02d10
Ajay Tiwari <atiwari2@wsgc.com>
2021-08-26 10:32:15 +0530

    Updates

---
+++ precache-manifest.70db369d8f59313ec59af04b5b195861.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b5a307eee09e097e6a1ae0157b36eb",
+    "url": "/aem-spa-2.0/index.html"
+  },
+  {
+    "revision": "5077d60bf71c5d1fcc05",
+    "url": "/aem-spa-2.0/static/css/main.5dd56545.chunk.css"
+  },
+  {
+    "revision": "7c9d0c6274b82f16ca49",
+    "url": "/aem-spa-2.0/static/js/2.2512a7c3.chunk.js"
+  },
+  {
+    "revision": "a112985e346e36813f8faa2e1e345c78",
+    "url": "/aem-spa-2.0/static/js/2.2512a7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5077d60bf71c5d1fcc05",
+    "url": "/aem-spa-2.0/static/js/main.5135e733.chunk.js"
+  },
+  {
+    "revision": "827124195cea8593a33f",
+    "url": "/aem-spa-2.0/static/js/runtime-main.58be2d71.js"
+  },
+  {
+    "revision": "dc08ac3660ecb449e6fc2c94f0d7c788",
+    "url": "/aem-spa-2.0/static/media/gspann-logo.dc08ac36.svg"
+  },
+  {
+    "revision": "f227c489ca8dcfded430ecc5a71d600f",
+    "url": "/aem-spa-2.0/static/media/icon-close.f227c489.svg"
+  },
+  {
+    "revision": "200bc7ed9cfe0f3bde2835852b50271e",
+    "url": "/aem-spa-2.0/static/media/icon-loading.200bc7ed.svg"
+  }
+]);
